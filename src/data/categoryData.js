5ad69cb33020a807cc53ba5eba0d439f4ff63212
nvkex <scrbwrm@gmail.com>
2021-02-05 19:22:35 +0530

    category data

---
+++ src/data/categoryData.js
@@ -0,0 +1,22 @@
+export const rootCategories = [
+    {
+        title: "Anime",
+        route: "/anime",
+        imgLink: "/assets/images/category-1.webp"
+    },
+    {
+        title: "Movies",
+        route: "/movies",
+        imgLink: "/assets/images/category-2.webp"
+    },
+    {
+        title: "TV Shows",
+        route: "/tv",
+        imgLink: "/assets/images/category-3.webp"
+    },
+    {
+        title: "Torrents",
+        route: "/torrents",
+        imgLink: "/assets/images/category-4.webp"
+    },
+]
