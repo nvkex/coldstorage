902030a83b75c3164787878d5198e8953af15e1b
nvkex <scrbwrm@gmail.com>
2021-02-05 14:40:17 +0530

    navigation list

---
+++ src/data/navbar_nav_list.js
@@ -0,0 +1,22 @@
+export const navlist = [
+    { 
+        title : "Home",
+        route : "/home" 
+    },
+    { 
+        title : "Anime",
+        route : "/anime" 
+    },
+    { 
+        title : "Torrents",
+        route : "/torrents" 
+    },
+    { 
+        title : "Movies",
+        route : "/movies" 
+    },
+    { 
+        title : "TV Shows",
+        route : "/tv" 
+    }
+]
