70ddce3ba9badc8774a4afc8867a2510ecf3e85b
nvkex <scrbwrm@gmail.com>
2021-02-11 14:43:57 +0530

    new route with container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import LandingPage from './components/LandingPage/LandingPage';
 import { Switch, Route } from 'react-router-dom';
 import LayoutOne from './containers/LayoutOne';
+import ItemView from './components/common/ItemView/ItemView';
+import CategoryView from './components/CategoryView/CategoryView';
 
 function App() {
   return (
     <div>
       <Switch>
-          <Route path="/c" component={LayoutOne} />
-          <Route exact path="/" component={LandingPage} />
+        <Route exact path="/" component={LandingPage} />
+        <LayoutOne>
+          <Route path="/c" component={CategoryView} />
+          <Route exact path="/id" component={ItemView} />
+        </LayoutOne>
+
       </Switch>
     </div>
   );
