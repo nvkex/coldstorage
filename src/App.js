35ec64b7306b38400db395fd399a395feeb7854a
nvkex <scrbwrm@gmail.com>
2021-02-16 20:05:18 +0530

    new routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import LayoutOne from './containers/LayoutOne';
 import ItemView from './components/common/ItemView/ItemView';
 import CategoryView from './components/CategoryView/CategoryView';
+import PageNotFound from './components/common/404/404';
 
 function App() {
   return (
@@ -10,10 +11,12 @@ function App() {
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <LayoutOne>
-          <Route path="/c" component={CategoryView} />
-          <Route exact path="/id" component={ItemView} />
+          <Switch>
+            <Route path="/c" component={CategoryView} />
+            <Route path="/id" component={ItemView} />
+            <Route path="/404" component={PageNotFound} />
+          </Switch>
         </LayoutOne>
-
       </Switch>
     </div>
   );
