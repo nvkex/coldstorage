8138f691a06c49b1a17de2412c4707e04763be27
nvkex <scrbwrm@gmail.com>
2021-02-24 21:22:59 +0530

    User Signup Page Boilerplate

---
+++ src/components/Auth/Signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class UserSignup extends Component {
+  render() {
+    return (
+      <div>
+        User Signup
+      </div>
+    )
+  }
+}
+
+export default UserSignup
