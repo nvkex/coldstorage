3467e2928d5cb5aa05983d76d50ae66af9876717
nvkex <scrbwrm@gmail.com>
2021-03-03 21:44:34 +0530

    card layout updates

--- src/components/common/components/SimpleCard.js
+++ src/components/common/components/SimpleCard.js
@@ -4,8 +4,8 @@ import classes from './SimpleCard.module.css'
 class SimpleCard extends Component {
     render() {
         return (
-            <div className="col-lg-4">
-                <div className={classes.card}>
+            <div className="col-lg-3 col-md-3 col-sm-6">
+                <div className={`${classes.card} shadow`}>
                     <a href={this.props.category.route}>
                         <img src={`${process.env.PUBLIC_URL}${this.props.category.imgLink}`} alt="category_img" />
                     </a>
