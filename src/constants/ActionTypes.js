d7a2121651649c05e7633ba84d1d1371bc616d84
nvkex <scrbwrm@gmail.com>
2021-03-21 15:47:25 +0530

    action constants

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,7 @@
+// AUthentication
+export const LOGIN_USER = 'LOGIN_USER';
+export const LOGOUT_USER = 'LOGOUT_USER';
+export const LOGIN_ADMIN = 'LOGIN_ADMIN';
+export const LOGOUT_ADMIN = 'LOGOUT_ADMIN';
+export const SAVE_DATA = 'SAVE_DATA';
+export const CLEAR_DATA = 'CLEAR_DATA';
